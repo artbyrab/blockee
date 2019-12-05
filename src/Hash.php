ab88881f3635958a7c411a89bed9b559ef0be813
RAB <contact@art-by-rab.com>
2019-12-05 13:04:04 +0000

    Remove the objectHash function as we no longer need it
    Now all hashing of blocks is done on a string version of the blocks data
    and the previous blocks hash.

--- src/Hash.php
+++ src/Hash.php
@@ -17,19 +17,6 @@ namespace artbyrab\blockee;
 class Hash
 {
     /**
-     * Object hash
-     * 
-     * This is used for hashing an object for example a Block.
-     * 
-     * @param object $object
-     * @return string The hash of the object.
-     */
-    public static function objectHash($object)
-    {
-        return spl_object_hash($object);
-    }
-
-    /**
      * String hash
      * 
      * This is used for hashing a string.
